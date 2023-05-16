d5e0fd750214f522ef017bbd4a00c5a68a206235
victorlamas <victor.lamas@udc.es>
2023-05-16 16:43:42 +0200

    added basic functionality

---
+++ examples/BasicExample.js
@@ -0,0 +1,16 @@
+import Processor from "../index.js";
+
+// Path to the folder with the shapefiles (cannot be a relative path)
+const folderPath = 'C:/Users/victor/Desktop/shapefiles/examples/shpfiles/';
+
+const processor = new Processor({
+    encoding: 'utf-8', // 'auto' by default || 'ascii' || 'utf8' || 'utf-8' || 'latin1' || 'binary' || 'base64' || 'hex'
+    geographicInfo: false, // true by default
+    records: false, // true by default
+});
+
+processor.getSHPFolderInfo(folderPath).then(
+    (content) => {
+        console.log(JSON.stringify(content, null, 2));
+    }
+)
