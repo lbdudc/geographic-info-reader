8cd4c094165eb90be57246b5f371185b6a473342
Juan Couselo <juancouselo@udc.es>
2025-06-18 10:17:14 +0200

    add tests to improve coverage

--- test/Processor.test.js
+++ test/Processor.test.js
@@ -64,6 +64,60 @@ describe("Processor", () => {
 
     expect(res).toStrictEqual(expectedJSONRes);
   });
+  test("Process folder with geographicInfo activated", async () => {
+    const testFolderPath = "./test/testData/processorGeographicInfo";
+    const inputFolderPath = "./test/testData/inputUnzipShapefile";
+
+    try {
+      rmdirSync(`${testFolderPath}/output`, { recursive: true, force: true });
+    } catch (error) {
+      // Expect that the error is because the folder doesn't exist
+      expect(error.code).toBe("ENOENT");
+    }
+
+    cpSync(`${inputFolderPath}`, `${testFolderPath}/input`, {
+      recursive: true,
+      force: true,
+    });
+
+    const processor = new Processor({
+      encoding: "utf-8", // 'auto' by default || 'ascii' || 'utf8' || 'utf-8' || 'latin1' || 'binary' || 'base64' || 'hex'
+      geographicInfo: true,
+      outputPath: `${testFolderPath}/`,
+    });
+
+    const res = await processor.processFolder(`${testFolderPath}/input`);
+
+    // Assert that the output folder contains the expected files
+    const expectedJSONRes = JSON.parse(
+      readFileSync(
+        `${testFolderPath}/expectedOutput/expectedOutput.json`,
+        "utf8",
+      ),
+    );
+    const expectedFiles = JSON.parse(
+      readFileSync(
+        `${testFolderPath}/expectedOutput/expectedFiles.json`,
+        "utf8",
+      ),
+    );
+
+    expect(existsSync(`${testFolderPath}/output`)).toBe(true);
+
+    const readFiles = readdirSync(`${testFolderPath}/output`);
+
+    for (const expectedFile of expectedFiles) {
+      expect(readFiles.includes(expectedFile)).toBe(true);
+    }
+
+    writeFileSync(
+      `${testFolderPath}/output/output.json`,
+      JSON.stringify(res, null, 2),
+      "utf8",
+    );
+
+    expect(res).toStrictEqual(expectedJSONRes);
+  });
   test("Process unzip shapefiles", async () => {
     const testFolderPath = "./test/testData/processorUnzipShapefile";
     const inputFolderPath = "./test/testData/inputUnzipShapeFile";
@@ -81,7 +135,6 @@ describe("Processor", () => {
     });
 
     const processor = new Processor({
-      encoding: "utf-8", // 'auto' by default || 'ascii' || 'utf8' || 'utf-8' || 'latin1' || 'binary' || 'base64' || 'hex'
       geographicInfo: false, // true by default
       outputPath: `${testFolderPath}/`,
     });
@@ -117,5 +170,30 @@ describe("Processor", () => {
     );
 
     expect(res).toStrictEqual(expectedJSONRes);
+  });
+  test("Input error", async () => {
+    const testFolderPath = "./test/testData/processorWrongInput";
+    const inputFolderPath = "./test/testData/wrongInput";
+
+    try {
+      rmdirSync(`${testFolderPath}/output`, { recursive: true, force: true });
+    } catch (error) {
+      // Expect that the error is because the folder doesn't exist
+      expect(error.code).toBe("ENOENT");
+    }
+
+    cpSync(`${inputFolderPath}`, `${testFolderPath}/input`, {
+      recursive: true,
+      force: true,
+    });
+
+    const processor = new Processor({
+      encoding: "utf-8", // 'auto' by default || 'ascii' || 'utf8' || 'utf-8' || 'latin1' || 'binary' || 'base64' || 'hex'
+      geographicInfo: false, // true by default
+      outputPath: `${testFolderPath}/`,
+    });
+
+    const res = await processor.processFolder(`${testFolderPath}/input`);
+    expect(res).toEqual([]);
   });
 });
